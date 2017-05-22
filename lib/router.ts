2fdc91dc18cac4ef52a03a841cc21e6df88eaa9a
Christophe Massolin <christophe.massolin@gmail.com>
2017-05-23 01:29:24 +0200

    [Router] Add concern and constraint

--- lib/router.ts
+++ lib/router.ts
@@ -6,6 +6,7 @@ class Router {
 
   public root:Route
   public currentScope:Route
+  public concerns = {}
 
   constructor() {
     this.root = new Route('/', null)
@@ -18,12 +19,20 @@ class Router {
     })
   }
 
-  route(path, closure:(parameters:any) => void) {
+  route(path, closure:(parameters:any) => void, options?:any) {
+    let route:Route
+
     if (!path || path.length === 0 || path === '/') {
       this.currentScope.closure = closure
+      route = this.currentScope
     } else {
-      return this.currentScope.findOrCreate(path, closure)
+      route = this.currentScope.findOrCreate(path, closure)
     }
+
+    if (options && options.constraint) this.addConstraint(route, options.constraint)
+    if (options && options.concern)    this.addConcern(route, options.concern)
+
+    return route
   }
 
   scope(path:string, closure:(parameters:any) => void) {
@@ -38,6 +47,61 @@ class Router {
     this.currentScope = current
 
     return scope
+  }
+
+  concern(name:string, closure:() => void) {
+    this.concerns[name] = closure
+  }
+
+  addConstraint(pathOrRoute:string | Route, constraint:((value:string) => boolean)|RegExp|string) {
+
+    let route:Route
+
+    if (typeof pathOrRoute === 'string') {
+      route = this.currentScope.findOrCreate(pathOrRoute as string)
+    } else {
+      route = pathOrRoute
+    }
+
+    const c = constraint
+    let regex:RegExp
+
+    if (typeof constraint != 'function') {
+      if (constraint instanceof RegExp) {
+        regex = c as RegExp
+      } else if (typeof constraint === 'string') {
+        regex = new RegExp(c as string)
+      }
+
+      constraint = function(value) {
+        return !!value.match(regex)
+      }
+    }
+
+    route.path.constraint = constraint as ((value:string) => boolean)
+  }
+
+  addConcern(pathOrRoute:string | Route, concern:string|string[]) {
+    let route:Route
+
+    if (typeof pathOrRoute === 'string') {
+      route = this.currentScope.findOrCreate(pathOrRoute as string)
+    } else {
+      route = pathOrRoute
+    }
+
+    if (typeof concern === 'string') concern = [ concern ]
+
+    for (let i = 0, ilen = concern.length; i < ilen; i++) {
+      const closure = this.concerns[concern[i]]
+
+      if (closure) {
+        const current = this.currentScope
+        this.currentScope = route
+        closure()
+        this.currentScope = current
+      }
+    }
   }
 
   go(path:string) {
--- test/test.ts
+++ test/test.ts
@@ -16,6 +16,31 @@ router.route('/hello', function() {
 
 // router.go('/hello')
 
+router.concern('errors', function() {
+
+  router.route('404', function() {
+    console.log('404')
+  })
+
+  router.route('500', function() {
+    console.log('500')
+  })
+
+})
+
+router.concern('classic', function() {
+
+  router.route('contact', function() {
+    console.log('contact')
+  })
+
+  router.route('about', function() {
+    console.log('about')
+  })
+
+})
+
+router.addConcern('/:country', [ 'errors' ])
 router.scope('/:country/:locale', function() {
 
   router.route('/', function(parameters) {
@@ -28,6 +53,8 @@ router.route('/', function() {
   console.log('home home')
 })
 
+
 router.go('/FR/fr')
+// router.go('/:country/:locale', { country: 'FR', locale: 'fr' })
 
 console.log(router.getRoutes())
