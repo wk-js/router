cab53277877b3164fc641fce186be00a8fc100e0
Christophe Massolin <christophe.massolin@makemepulse.com>
2017-05-31 20:12:15 +0200

    Maybe fix fuse-box compilation?

--- fuse.js
+++ fuse.js
@@ -1,15 +1,28 @@
 'use strict'
 
-const { FuseBox, StylusPlugin } = require('fuse-box')
+const { FuseBox, UglifyJSPlugin } = require('fuse-box')
 
 const fuse = FuseBox.init({
   homeDir: "lib",
-  output: "dist/$name.js"
+  output: "dist/$name.js",
+  tsConfig: `tsconfig.json`,
+  useCache: false
 })
 
-fuse.bundle('main')
-  .instructions('> index.ts')
-  .watch()
+if (process.env.NODE_ENV === 'release') {
+  fuse.bundle('main')
+      .instructions('>index.ts')
 
-fuse.dev()
+  fuse.bundle('main.min.js')
+      .instructions('> index.ts')
+      .plugin( UglifyJSPlugin() )
+} else {
+  fuse.bundle('main')
+      .tsConfig('tsconfig.json')
+      .instructions('> index.ts')
+      .watch()
+
+  fuse.dev()
+}
+
 fuse.run()
